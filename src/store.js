2caac75e8164b09eb58f7c3f201f6f26c2af55c9
deepaksh-mca <deepakmca207@gmail.com>
2025-09-08 15:26:11 -0400

    code commit

--- src/store.js
+++ src/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
 
-const calculateTotalQuantity = () => {
- return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
-  };
+
   
  const store = configureStore({
     reducer: {
